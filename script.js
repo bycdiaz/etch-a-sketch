592c0edf8f2bec28e09189e11175c32b83c9af75
bycdiaz <cdiaz86@gmail.com>
2019-02-03 11:44:40 -0500

    completed files.

---
+++ script.js
@@ -0,0 +1,42 @@
+const gridContainer = document.querySelector('.container');
+
+function gridInit(gridDensity=16) {
+  createGrid(gridDensity);
+  hoverChange();
+  resetGrid();
+}
+
+gridInit();
+
+function createGrid(gridDensity) {
+  console.log(gridDensity);
+  for (let i = 0; i < gridDensity * gridDensity; i++) {
+    const gridItems = document.createElement('div');
+    gridItems.className = "grid-item";
+    // gridItems.innerText = i + 1;
+    gridContainer.appendChild(gridItems);
+  }
+  gridContainer.style.gridTemplateColumns = `repeat(${gridDensity}, 1fr)`;
+  gridContainer.style.gridTemplateRows = `repeat(${gridDensity}, 1fr)`
+};
+
+// toggles class
+function hoverChange() {
+  const gridCells = document.querySelectorAll('.grid-item');
+  gridCells.forEach( gridCell => {
+      gridCell.addEventListener('mouseover', function(e) {
+      e.target.classList.toggle("grid-item-hovered");
+    }, {once: true});
+  });
+};
+
+function resetGrid() {
+  const freshGrid = document.querySelector('#resetGrid');
+  freshGrid.addEventListener('click', function(e) {
+    document.querySelector('.container').innerHTML = "";
+    const gridDensity = Number(prompt("Please enter number of rows and columns:"));
+    // const gridDensity = requestedGridDensity * requestedGridDensity;
+    gridInit(gridDensity);
+  });
+
+}
